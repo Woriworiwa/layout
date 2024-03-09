d1a5c7970e4ea29c0f452e3d523e52f5879ee198
Omar Dalgamuni <omar.dalgamuni@better.care>
2024-03-09 12:30:13 +0100

    added nebular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {NbSidebarModule, NbThemeModule} from "@nebular/theme";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(NbThemeModule.forRoot({
+      name: 'default',
+    })),
+    importProvidersFrom(NbSidebarModule.forRoot())
+  ]
 };
