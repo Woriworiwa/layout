e614db41aef5335734fe4c8e0568d02baff9ef51
Omar Dalgamuni <omar@dalgamuni.me>
2026-01-13 21:32:44 +0100

    feat(properties): Create a library

---
+++ libs/properties/src/lib/properties/properties.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'properties-properties',
+  imports: [],
+  templateUrl: './properties.component.html',
+  styleUrl: './properties.component.scss',
+})
+export class PropertiesComponent {}
